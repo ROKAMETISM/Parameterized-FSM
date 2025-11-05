d9a2246aa007b460c04cc57d8be7e19e1b03899e
ROJS <mskim829@naver.com>
2025-11-05 23:29:23 +0900

    Define

---
+++ Dummy/dummy.gd
@@ -0,0 +1,13 @@
+class_name Card extends CharacterBody2D
+
+
+
+func _physics_process(delta: float) -> void:
+	
+	
+	
+	
+	
+	
+	return
+	
--- test_script.gd
+++ test_script.gd
@@ -1,10 +1,1 @@
-extends Node
-
-
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass
-
-func _unhandled_input(event: InputEvent) -> void:
-	if Input.is_action_just_pressed("left_click"):
-		pass
+extends RefCounted
