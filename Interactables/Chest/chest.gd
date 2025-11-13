b687bcf32166f06aa2138a11a5be7638f07824b5
ROJS <mskim829@naver.com>
2025-11-13 23:29:45 +0900

    Studying Shader 3

--- Interactables/Chest/chest.gd
+++ Interactables/Chest/chest.gd
@@ -6,8 +6,6 @@ extends StaticBody2D
 
 var vis  = Preloads.SCENE_INVENTORY_UI.instantiate()
 
-var test_bool := false
-var test_timer := 1.0
 
 func _ready() -> void:
 	inventory.add_item(Preloads.ITEM_APPLE, 32)
@@ -16,10 +14,3 @@ func _ready() -> void:
 func _on_interacted(source:Node2D)->void:
 	if source.has_method("access_inventory"):
 		source.access_inventory(inventory)
-
-func _process(delta: float) -> void:
-	test_timer -= delta
-	if test_timer < 0:
-		test_timer += 1.0
-		test_bool = not test_bool
-		sprite.material.set_shader_parameter("enable_vertex", test_bool)
